eccb963941b4c5578047fe22c47a855f788e9916
jacksondonascimento <127577213+jacksondonascimento@users.noreply.github.com>
2023-05-29 20:32:50 -0400

    Add files via upload

---
+++ calculadora trabalho/Calculadora.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadora_trabalho
+{
+    public partial class Calculadora : Form
+    {
+        decimal valor1 = 0, valor2 = 0;
+        string operacao = "";
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "2";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "3";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "4";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "5";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "6";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "7";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "8";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + "9";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "DIVIDIR";
+                lblOperacao.Text = "/";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "MULTIPLICAR";
+                lblOperacao.Text = "*";
+            }
+        }
+        
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SUBTRAIR";
+                lblOperacao.Text = "-";
+            }
+        }    
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (txtResultado.Text != "")
+            {
+                valor1 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+                txtResultado.Text = "";
+                operacao = "SOMA";
+                lblOperacao.Text = "+";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(txtResultado.Text, CultureInfo.InvariantCulture);
+            if (operacao == "SOMA")
+            {
+                txtResultado.Text = Convert.ToString(valor1 + valor2);
+            }
+            else if (operacao == "SUBTRAIR")
+            {
+                txtResultado.Text = Convert.ToString(valor1 - valor2);
+            }
+            else if (operacao == "MULTIPLICAR")
+            {
+                txtResultado.Text = Convert.ToString(valor1 * valor2);
+            }
+            else if (operacao == "DIVIDIR")
+            {
+                txtResultado.Text = Convert.ToString(valor1 / valor2);
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+            lblOperacao.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = txtResultado.Text + ".";
+        }
+    }
+}
